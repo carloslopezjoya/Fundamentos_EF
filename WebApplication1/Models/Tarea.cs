ef4747afbb80256ddc233aa8ec638c86f630ac85
Carlos Eduardo Lopez Joya <clopezj@credifinanciera.com.co>
2023-03-06 16:28:23 -0500

    Conexion de base de datos en memoria

--- WebApplication1/Models/Tarea.cs
+++ WebApplication1/Models/Tarea.cs
@@ -1,14 +1,23 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
 {
     public class Tarea
     {
+        [Key]
         public Guid TareaId { get; set; }
-        public Guid CategoriaId { get; set; }   
+        [ForeignKey("CategoriaId")]
+        public Guid CategoriaId { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Titulo { get; set; }
         public Prioridad Prioridad { get; set; }
         public DateTime FechaCreacion { get; set; } 
         public virtual Categoria Categoria { get; set; }
-        
+        [NotMapped]
+        public string Resumen { get; set; }
+
 
 
     }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,13 +1,25 @@
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<TareasContext>(opt => opt.UseInMemoryDatabase("TareasDB"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+    {
+        dbContext.Database.EnsureCreated();
+        return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+    });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
