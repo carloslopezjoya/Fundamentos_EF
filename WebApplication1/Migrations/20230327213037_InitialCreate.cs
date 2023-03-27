08131bcb4217df75794bcb1c9ce0888a9972b53d
CARLOS LOPEZ <carlos.653@hotmail.com>
2023-03-27 16:32:57 -0500

    migration

---
+++ WebApplication1/Migrations/20230327213037_InitialCreate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nombre = table.Column<string>(type: "varchar(150)", maxLength: 150, nullable: false),
+                    Descripcion = table.Column<string>(type: "varchar", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tarea",
+                columns: table => new
+                {
+                    TareaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoriaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false),
+                    Prioridad = table.Column<int>(type: "int", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "Date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarea", x => x.TareaId);
+                    table.UniqueConstraint("AK_Tarea_CategoriaId", x => x.CategoriaId);
+                    table.ForeignKey(
+                        name: "FK_Tarea_Categoria_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categoria",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarea");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+        }
+    }
+}
